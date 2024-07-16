ce2983895307305ca1c66e2dee95dbfe1d7aed11
AstroNinja389 <nizubrilina@gmail.com>
2024-07-16 10:10:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -84,15 +84,10 @@ io.on('connection', (socket) => {
 
 
 const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+ 
