518d25e5b93f41097515e36fb0287d7dc0c24ea0
AstroNinja389 <nizubrilina@gmail.com>
2024-07-16 11:20:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
