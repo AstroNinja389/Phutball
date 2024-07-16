61514a43973406ce2a25e8a12438e114ba480cc5
AstroNinja389 <nizubrilina@gmail.com>
2024-07-16 11:23:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -85,10 +85,6 @@ io.on('connection', (socket) => {
 });
 
 
-
-const path = require('path');
-
-
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
